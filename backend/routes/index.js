572240da991b8b4b5000141e1ee6b3863f851d16
Adarsh Tripath <tripathiadarsh39@gmail.com>
2020-11-06 21:57:43 +0530

    added gitignore and routes

---
+++ backend/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res) => {
+    res.send('Hello World');
+});
+
+//Adding New Vender
+
+router.post('/addvendor', )
+
+module.exports = router
