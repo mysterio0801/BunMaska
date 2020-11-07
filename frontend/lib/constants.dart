da3eb707a9040ad874e30f17e550d950bd62bf44
AnuVe <anushkavermatheprincess@gmail.com>
2020-11-07 15:32:30 +0530

    ui changes

--- frontend/lib/constants.dart
+++ frontend/lib/constants.dart
@@ -7,7 +7,7 @@ class OurTheme {
       appBarTheme: AppBarTheme(color: Colors.orange[400]),
       //Button Theme
       buttonTheme: ButtonThemeData(
-        buttonColor: Colors.black,
+        buttonColor: Colors.orange[400],
         padding: EdgeInsets.symmetric(horizontal: 20.0),
         minWidth: 100.0,
         height: 40.0,
--- frontend/lib/screens/welcome_screen.dart
+++ frontend/lib/screens/welcome_screen.dart
@@ -13,20 +13,37 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xfffcf5ee),
-      appBar: AppBar(title: Text('BunMaska'),),
-      body:Column(children: [
+      appBar: AppBar(
+        title: Text('BunMaska'),
+      ),
+      body: Column(
+        children: [
           Expanded(
             child: ListView(
               children: [
-                Image(image: AssetImage('images/bun_maska.png'),),
+                Image(
+                  image: AssetImage('images/bun_maska.png'),
+                ),
                 SizedBox(height: 20.0),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: RaisedButton(onPressed: null, child: Text('Distributor', style: TextStyle(color: Colors.white),),),
+                  child: RaisedButton(
+                    onPressed: null,
+                    child: Text(
+                      'Distributor',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(25.0),
-                  child: RaisedButton(onPressed: null, child: Text('Consumer', style: TextStyle(color: Colors.white),),),
+                  child: RaisedButton(
+                    onPressed: null,
+                    child: Text(
+                      'Consumer',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
                 ),
               ],
             ),
