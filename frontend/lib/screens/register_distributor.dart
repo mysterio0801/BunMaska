7edf6553fac2f8bca32cd40174c994d01d221986
Adarsh Tripath <tripathiadarsh39@gmail.com>
2020-11-09 20:35:11 +0530

    added form validation

--- frontend/lib/screens/register_distributor.dart
+++ frontend/lib/screens/register_distributor.dart
@@ -3,6 +3,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/distributor_home.dart';
 import 'package:http/http.dart' as http;
+import 'package:form_field_validator/form_field_validator.dart';
 
 class RegisterDistributor extends StatefulWidget {
   @override
@@ -10,12 +11,120 @@ class RegisterDistributor extends StatefulWidget {
 }
 
 class _RegisterDistributorState extends State<RegisterDistributor> {
-  TextEditingController _ownerName = TextEditingController();
-  TextEditingController _shopName = TextEditingController();
-  TextEditingController _contact = TextEditingController();
-  TextEditingController _address = TextEditingController();
-  TextEditingController _password = TextEditingController();
-  TextEditingController _email = TextEditingController();
+  String baseUrl = "https://chai-app.herokuapp.com/";
+
+  String _ownerName;
+  String _shopName;
+  String _contact;
+  String _address;
+  String _password;
+  String _email;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Widget _buildOwnerName() {
+    return TextFormField(
+      decoration: textfieldDecor('Owner Name', Icon(Icons.person)),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return "Owner Name is required";
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _ownerName = value;
+      },
+    );
+  }
+
+  Widget _buildShopName() {
+    return TextFormField(
+      decoration: textfieldDecor('Shop Name', Icon(Icons.shop)),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return "Shop Name is required";
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _shopName = value;
+      },
+    );
+  }
+
+  Widget _buildContact() {
+    return TextFormField(
+      decoration: textfieldDecor('Contact No.', Icon(Icons.contact_phone)),
+      keyboardType: TextInputType.phone,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return "Contact No. is required";
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _contact = value;
+      },
+    );
+  }
+
+  Widget _buildAddress() {
+    return TextFormField(
+      decoration: textfieldDecor('Address', Icon(Icons.location_city_outlined)),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return "Address is required";
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _address = value;
+      },
+    );
+  }
+
+  Widget _buildPassword() {
+    return TextFormField(
+      decoration: textfieldDecor('Password', Icon(Icons.lock_outline)),
+      keyboardType: TextInputType.visiblePassword,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return "Password is required";
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _password = value;
+      },
+    );
+  }
+
+  Widget _buildEmail() {
+    return TextFormField(
+      decoration: textfieldDecor('Enter your Email', Icon(Icons.person)),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return "Email is required";
+        }
+        if (!RegExp(
+                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+            .hasMatch(value)) {
+          return "Please enter a valid email address";
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _email = value;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,88 +151,68 @@ class _RegisterDistributorState extends State<RegisterDistributor> {
                   ),
                 ),
                 SizedBox(height: 45.0),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 5.0),
-                  child: TextFormField(
-                    controller: _ownerName,
-                    decoration:
-                        textfieldDecor('Owner Name', Icon(Icons.person)),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 5.0),
-                  child: TextFormField(
-                    controller: _shopName,
-                    decoration: textfieldDecor('Shop Name', Icon(Icons.shop)),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 5.0),
-                  child: TextFormField(
-                    controller: _contact,
-                    decoration: textfieldDecor(
-                        'Contact Number', Icon(Icons.contact_phone)),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 5.0),
-                  child: TextFormField(
-                    controller: _address,
-                    decoration: textfieldDecor(
-                        'Address', Icon(Icons.location_city_outlined)),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 5.0),
-                  child: TextFormField(
-                    controller: _email,
-                    decoration:
-                        textfieldDecor('Email', Icon(Icons.email_outlined)),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 10.0),
-                  child: TextFormField(
-                    controller: _password,
-                    obscureText: true,
-                    decoration:
-                        textfieldDecor('Password', Icon(Icons.lock_outline)),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 40.0, vertical: 40.0),
-                  child: RaisedButton(
-                    onPressed: () async {
-                      Map<String, String> data = {
-                        "shopName": _shopName.text,
-                        "ownerName": _ownerName.text,
-                        "contact": _contact.text,
-                        "address": _address.text,
-                        "password": _password.text,
-                        "email": _email.text
-                      };
-                      var response = await http.post(
-                          "https://chai-app.herokuapp.com/addUser",
-                          body: data);
+                Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 10.0),
+                            child: _buildOwnerName()),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 10.0),
+                            child: _buildShopName()),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 10.0),
+                            child: _buildEmail()),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 10.0),
+                            child: _buildContact()),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 10.0),
+                            child: _buildAddress()),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 10.0),
+                            child: _buildPassword()),
+                        SizedBox(height: 35.0),
+                        RaisedButton(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 160.0, vertical: 5.0),
+                            child: Text(
+                              'Register',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () async {
+                              if (!_formKey.currentState.validate()) {
+                                return null;
+                              }
 
-                      return Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DistributorHomeScreen()));
-                    },
-                    child: Text(
-                      'Register',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
+                              _formKey.currentState.save();
+
+                              Map<String, String> data = {
+                                "shopName": _shopName,
+                                "ownerName": _ownerName,
+                                "contact": _contact,
+                                "address": _address,
+                                "password": _password,
+                                "email": _email
+                              };
+                              var response = await http
+                                  .post(baseUrl + "addUser", body: data);
+
+                              return Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          DistributorHomeScreen()));
+                            }),
+                      ],
+                    )),
               ],
             ),
           ),
